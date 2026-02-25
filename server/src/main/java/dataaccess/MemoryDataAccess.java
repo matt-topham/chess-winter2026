7ad87ddbe65a0d9217a46d5ee919ae7f2430b713
mdtopham@gmail.com <mdtopham@gmail.com>
2026-02-25 09:27:25 -0700

    added skeleton for each of the functions I need to write

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -5,22 +5,70 @@ import model.UserData;
 import model.GameData;
 import chess.ChessGame;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MemoryDataAccess {
+public class MemoryDataAccess implements DataAccess {
 
     private final Map<String, UserData> usersByUsername = new ConcurrentHashMap<>();
     private final Map<String, AuthData> authByToken = new ConcurrentHashMap<>();
     private final Map<Integer, GameData> gamesById = new ConcurrentHashMap<>();
     private final AtomicInteger nextGameId = new AtomicInteger(1);
 
+    @Override
     public void clear() throws DataAccessException {
         usersByUsername.clear();
         authByToken.clear();
         gamesById.clear();
         nextGameId.set(1);
+    }
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public int insertGame(GameData data) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
     }
 
 }
