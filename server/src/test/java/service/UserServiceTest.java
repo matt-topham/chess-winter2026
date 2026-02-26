716311f57e847f02ed9f380be52109e39fa603d9
mdtopham@gmail.com <mdtopham@gmail.com>
2026-02-26 09:22:02 -0700

    created the positive test for the register service

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -12,6 +12,23 @@ public class UserServiceTest {
 
     @Test
     void registerPositiveCreateUsersAndAuth() throws Exception {
+        DataAccess data = new MemoryDataAccess();
+        UserService service = new UserService(data);
+
+        RegisterRequest request = new RegisterRequest("matt", "pw", "matt@email.com");
+        RegisterResult result = service.register(request);
+
+        assertEquals("matt", result.username());
+        assertNotNull(result.authToken());
+        assertFalse(result.authToken().isBlank());
+
+        UserData user = data.getUser("matt");
+        assertNotNull(user);
+        assertEquals("matt@email.com", user.email());
+
+        AuthData auth = data.getAuth(result.authToken());
+        assertNotNull(auth);
+        assertEquals("matt", auth.username());
 
     }
 
