04e093d25575fe0ad502cdb3aafc51d384d3448b
mdtopham@gmail.com <mdtopham@gmail.com>
2026-02-26 08:40:29 -0700

    created framework for UserServiceTest

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,22 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Test
+    void registerPositiveCreateUsersAndAuth() throws Exception {
+
+    }
+
+    @Test
+    void registerNegativeAlreadyTaken() throws Exception {
+
+    }
+}
