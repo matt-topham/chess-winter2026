d865e55f8d2dab6c48edb07be31cc51f3be79665
mdtopham@gmail.com <mdtopham@gmail.com>
2026-03-09 13:27:49 -0600

    created MySqlDataAccess.java and had it auto implement many of the functions as placeholders for now

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,60 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MySqlDataAccess implements DataAccess{
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public int insertGame(GameData data) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+}
