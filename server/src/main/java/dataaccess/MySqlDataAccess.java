55fdd06a86cbe76610624c4fefdda5ed13306926
mdtopham@gmail.com <mdtopham@gmail.com>
2026-03-10 11:15:10 -0600

    added a helper function and teh clear function to MySqlDataAccess.java

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
@@ -8,9 +9,21 @@ import java.util.Collection;
 import java.util.List;
 
 public class MySqlDataAccess implements DataAccess{
+
+    private final Gson gson = new Gson();
+
     @Override
     public void clear() throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection();
+             var statement = connection.createStatement()) {
 
+            statement.executeUpdate("DELETE FROM auth");
+            statement.executeUpdate("DELETE FROM game");
+            statement.executeUpdate("DELETE FROM user");
+
+        } catch (Exception e) {
+            throw dbError("clear", e);
+        }
     }
 
     @Override
@@ -56,5 +69,9 @@ public class MySqlDataAccess implements DataAccess{
     @Override
     public void updateGame(GameData game) throws DataAccessException {
 
+    }
+
+    private DataAccessException dbError(String where, Exception e) {
+        return new DataAccessException(where + ": " + e.getMessage(), e);
     }
 }
