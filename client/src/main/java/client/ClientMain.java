a676175d22fda3cd2000f0eb35cfb5e49905c205
mdtopham@gmail.com <mdtopham@gmail.com>
2026-03-23 15:19:05 -0600

    Added the run method and placeholder methods for now to handle prelogin and postlogin

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,16 @@ package client;
 
 import chess.*;
 
+import java.util.Scanner;
+
 public class ClientMain {
 
     private final ServerFacade facade;
+    private final Scanner in = new Scanner(System.in);
+
+    private enum State {PRELOGIN, POSTLOGIN}
+
+    private State state = State.PRELOGIN;
 
     public ClientMain(String host, int port) {
         this.facade =  new ServerFacade(host, port);
@@ -22,5 +29,49 @@ public class ClientMain {
 
     private void run() {
         System.out.println("♕ 240 Chess Client");
+        preloginHelp();
+
+        while (true) {
+            System.out.print(prompt());
+            String line = in.nextLine().trim();
+            if (line.isEmpty()) continue;
+
+            try {
+                if (state == State.PRELOGIN) {
+                    if (handlePrelogin(line)) return;
+                }
+                else {
+                    handlePostlogin(line);
+                }
+            }
+            catch (ServerFacade.ClientException e) {
+                System.out.println(cleanMessage(e.getMessage()));
+            }
+            catch (Exception e) {
+                System.out.println("An error occurred. Please try again.");
+            }
+        }
+    }
+
+    private boolean handlePrelogin(String line) throws Exception {
+        // come back and fill in
+        return false;
+    }
+
+    private void preloginHelp() {
+
+    }
+
+    private void handlePostlogin(String line) throws Exception {
+
+    }
+
+    private String prompt() {
+        return (state == State.PRELOGIN) ? "[prelogin] >>> " : "[postlogin] >>> ";
+    }
+
+    private static String cleanMessage(String msg) {
+        // come back and finish this
+        return "Error: " + msg;
     }
 }
