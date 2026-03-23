1c3ccdf9a6fe56332f6a02a1546cb616c51918d0
mdtopham@gmail.com <mdtopham@gmail.com>
2026-03-23 15:04:07 -0600

    Added the main function to ClientMain. Working of REPL

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -3,8 +3,24 @@ package client;
 import chess.*;
 
 public class ClientMain {
+
+    private final ServerFacade facade;
+
+    public ClientMain(String host, int port) {
+        this.facade =  new ServerFacade(host, port);
+    }
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String host = "localhost";
+        int port = 8080;
+
+        if (args.length >= 1) host = args[0];
+        if (args.length >= 2) port = Integer.parseInt(args[1]);
+
+        new ClientMain(host, port).run();
+    }
+
+    private void run() {
+        System.out.println("♕ 240 Chess Client");
     }
 }
