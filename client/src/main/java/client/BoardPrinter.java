884a933346463c0ccfd0c9efb17aa6691fe86736
mdtopham@gmail.com <mdtopham@gmail.com>
2026-03-24 10:41:43 -0600

    filled in printInitialBoard method

--- client/src/main/java/client/BoardPrinter.java
+++ client/src/main/java/client/BoardPrinter.java
@@ -1,7 +1,14 @@
 package client;
 
+import chess.ChessBoard;
 import chess.ChessGame;
 
 public class BoardPrinter {
-    public static void printInitialBoard(ChessGame.TeamColor perspective) {}
+    public static void printInitialBoard(ChessGame.TeamColor perspective) {
+        ChessGame game = new ChessGame();
+        game.getBoard().resetBoard();
+        printBoard(game.getBoard(), perspective);
+    }
+
+    public static void printBoard(ChessBoard board, ChessGame.TeamColor perspective) {}
 }
